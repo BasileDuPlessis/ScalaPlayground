8dc1e30c8a84fc5f220a3e556b6ce9935413a1a2
Basile du Plessis <basile.duplessis@gmail.com>
2015-03-16 16:19:10 +0000

    Refactor Romain

--- src/main/scala/romain/Romain.scala
+++ src/main/scala/romain/Romain.scala
@@ -7,11 +7,12 @@ object Romain {
     val romainSymbol = Vector("I", "V", "X", "L", "C", "D", "M")
 
     def fromArabic(n: Int):String = n.toString.reverse.zipWithIndex.map{ t => 
+        val k = t._2 * 2
         t._1.asDigit match {
-            case i:Int if i<4 => romainSymbol(t._2 * 2) * i
-            case i:Int if i==4 => romainSymbol(t._2 * 2) + romainSymbol(t._2 * 2 + 1)
-            case i:Int if i<9 => romainSymbol(t._2 * 2 + 1) + romainSymbol(t._2 * 2) * (i-5)
-            case i:Int if i==9 => romainSymbol(t._2 * 2) + romainSymbol(t._2 * 2 + 2)
+            case i:Int if i<4 => romainSymbol(k) * i
+            case i:Int if i==4 => romainSymbol(k) + romainSymbol(k + 1)
+            case i:Int if i<9 => romainSymbol(k + 1) + romainSymbol(k) * (i-5)
+            case i:Int if i==9 => romainSymbol(k) + romainSymbol(k + 2)
         }
     }.reverse.mkString
     
